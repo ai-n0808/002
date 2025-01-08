a8e1e8c3dfcc87abfbdb24e3522e22e4a1742c28
ai-n0808 <aichan.88.aichan@gmail.com>
2025-01-08 11:52:07 +0900

    feat: implement all component and styling

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
+import "../css/Home.css";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useEffect([]);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  }, []);
+
+  function hundleSearch(e) {
+    e.preventDefault();
+    setSearchQuery("");
+  }
+
+  return (
+    <div className="home">
+      <form onSubmit={hundleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies ..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
